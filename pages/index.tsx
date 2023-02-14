37ca52c89649e4f8d7009e969ed82823d857be95
Alexander Putilin <avp-13@yandex.ru>
2023-02-14 23:21:51 +0000

    Random chess playing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,59 @@
 import { Chessboard } from 'react-chessboard';
+import { useCallback, useState } from 'react';
+import { Chess } from 'chess.js';
 
 
-export default function Home() {
+export default function GameOfChess() {
+  const [game, setGame] = useState(new Chess());
+
+  const safeGameMutate = useCallback((modify: (game: Chess) => void): void => {
+    setGame((g: Chess) => {
+      const update: Chess = new Chess(g.fen());
+      modify(update);
+      return update;
+    });
+  }, [setGame]);
+
+  const makeRandomMove = useCallback(() => {
+    safeGameMutate((game: Chess) => {
+      const possibleMoves = game.moves();
+      const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+
+      // exit if the game is over
+      if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0) return;
+
+      console.log("makeRnadomMove game", game);
+      game.move(possibleMoves[randomIndex]);
+    });
+  }, [safeGameMutate]);
+
+  const onDrop = useCallback((sourceSquare: string, targetSquare: string) => {
+    const gameCopy = new Chess(game.fen());
+
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+    setGame(gameCopy);
+
+    // illegal move
+    if (move === null) return false;
+
+    setTimeout(makeRandomMove, 250);
+    return true;
+  }, [game, setGame, makeRandomMove]);
+
+
   return <div className="container p-2">
     <div className="text-4xl font-bold mb-5">
       <h1>GPT chess</h1>
     </div>
     <div className="w-[700px] h-[700px]">
-      <Chessboard id="BasicBoard" />
+      <Chessboard
+        position={game.fen()}
+        onPieceDrop={onDrop}
+      />
     </div>
   </div>;
 
